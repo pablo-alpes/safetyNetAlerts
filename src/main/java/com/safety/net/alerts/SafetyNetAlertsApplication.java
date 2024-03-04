77dcc62e70dc014a61bbebdb14de6b483a3bd9a0
PabloMiranda <pablo.miranda@sfr.fr>
2024-03-04 19:41:14 +0100

    Project initialization.

---
+++ src/main/java/com/safety/net/alerts/SafetyNetAlertsApplication.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * Safety Net Alerts Program Launcher based on Spring
+ * @author: Pablo Miranda
+ *
+ */
+
+package com.safety.net.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+	}
+
+}
